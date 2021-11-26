27a2fc9f9475050fe30a7cc87bf8159f8f650fa2
ailerondeveloperindo <ailerondeveindo@gmail.com>
2021-11-26 13:37:21 +0800

    First Commit

---
+++ app/Views/homepage.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Aplikasi Manajemen UMKM Paling Lengkap - Biko</title>
+</head>
+<body>
+    <div class="container p-0 m-0">
+        <section class="section section--float-center bg-primary">
+            <div class="box-content text-center text-white pb-5">
+                <h1 class="display-3"> Satu Aplikasi Untuk Kemajuan Bisnis </h1>
+                <p class="lead"> Platform multi-fungsi yang memberikan solusi dalam ber-wirausaha</p>
+                <div class="btn btn-secondary"> Coba Sekarang </div>
+            </div>
+        </section>
+
+        <section class="section section--float-center">
+            <div class="box-content text-center px-3 text-primary">
+                <div class="row d-flex flex-row">
+                    <div class="col-4 d-flex flex-column">
+                        <h1 class="display-4"> Kenalan Sama Biko</h1>
+                        <p class="lead"> Biko itu seperti teman dekat anda, membantu disaat yang susah dan juga reliable.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section section--float-center">
+            <div class="box-content text-center px-3 text-primary">
+                <div class="row d-flex flex-row-reverse">
+                    <div class="col-4 d-flex flex-column">
+                        <h1 class="display-4"> Satu Platform Banyak Solusi</h1>
+                        <p class="lead"> Platform Biko membantu anda dalam mengatur keuangan, karyawan, inventori, dan sebagainya.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section section--float-center">
+            <div class="box-content text-center px-3 text-primary">
+                <div class="row">
+                    <div class="col-4 d-flex flex-column">
+                        <h1 class="display-4"> Bantu Pekerjaan anda Dengan AI</h1>
+                        <p class="lead"> Pekerjaan seperti menambahkan stok dari struk lebih gampang dengan Kecerdasaan Buatan</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Daftar Fitur Section -->
+        <section class="section">
+            <div class="d-flex flex-wrap justify-content-between px-5">
+                <div class="info-card px-1 py-4">
+                    <div class="info-card__img mb-4 bg-secondary"></div>
+                    <div class="info-card__title text-center">
+                        <h5>Pantau Kegiatan Usaha dari berbagai Perangkat</h5>
+                    </div>
+                </div>
+                <div class="info-card px-1 py-4">
+                    <div class="info-card__img mb-4 bg-secondary"></div>
+                    <div class="info-card__title text-center">
+                        <h5>Kontrol Kegiatan Usaha dengan mudah melalui Biko</h5>
+                    </div>
+                </div>
+                <div class="info-card px-1 py-4">
+                    <div class="info-card__img mb-4 bg-secondary"></div>
+                    <div class="info-card__title text-center">
+                        <h5>Bekerja Tanpa Khawatir dengan Kasir Online dan Offline</h5>
+                    </div>
+                </div>
+                <div class="info-card px-1 py-4">
+                    <div class="info-card__img mb-4 bg-secondary"></div>
+                    <div class="info-card__title text-center">
+                        <h5>Mengatur Karyawan Lebih Mudah</h5>
+                    </div>
+                </div>
+                <div class="info-card px-1 py-4">
+                    <div class="info-card__img mb-4 bg-secondary"></div>
+                    <div class="info-card__title text-center">
+                        <h5>Sistem Poin untuk kepuasaan pelanggan anda</h5>
+                    </div>
+                </div>
+                <div class="info-card px-1 py-4">
+                    <div class="info-card__img mb-4 bg-secondary"></div>
+                    <div class="info-card__title text-center">
+                        <h5>Fitur AI yang mengefisiensikan waktu anda</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="section__half-bg"></div>
+        </section>
+        <!-- End of Daftar Fitur Section -->
+
+        <!-- Testimoni Section -->
+        <section class="section bg-primary">
+    
+        </section>
+        <!-- End Of Testimoni Section -->
+
+        <!-- Testimoni Section -->
+        <section class="section bg-primary">
+    
+        </section>
+        <!-- End Of Testimoni Section -->
+
+        <!-- Footer Section -->
+        <footer class="footer">
+            <div class="footer--upper row px-5 py-4">
+                <div class="row">
+                    <div class="col-auto">
+                        <img src=<?= base_url("/assets/img/logo.svg") ?> alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-auto">
+                        <p>Business Kompanion</p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer--lower row px-5 py-4">
+                <div class="row">
+                    <div class="col-auto">
+                        <p class="m-0">2021 © Aileron Development</p>
+                    </div>
+                    <div class="col-auto">
+                        <p class="m-0">Privasi & Kebijakan</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Of Footer Section -->
+
+    </div>
+
+    <script src=<?= base_url("/assets/js/biko.bundle.js") ?> ></script>
+</body>
+</html>
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Button from 'bootstrap/js/dist/button';
+import Tooltip from 'bootstrap/js/dist/tooltip';
+import Dropdown from 'bootstrap/js/dist/dropdown';
+import './index.scss';
